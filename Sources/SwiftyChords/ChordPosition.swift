6cb883bda1af6e4fe89d4f3db53f27d9d15246b7
Michael Dimitrov <94137570+MichaelDimi@users.noreply.github.com>
2023-07-20 12:12:40 -0600

    Update txtFont of fret to be the size of the fret margin

--- Sources/SwiftyChords/ChordPosition.swift
+++ Sources/SwiftyChords/ChordPosition.swift
@@ -162,9 +162,9 @@ public struct ChordPosition: Codable, Identifiable, Equatable {
             if baseFret != 1 {
                 let txtLayer = CAShapeLayer()
                 #if os(iOS)
-                let txtFont = UIFont.systemFont(ofSize: fretConfig.margin * 0.5)
+                let txtFont = UIFont.systemFont(ofSize: fretConfig.margin)
                 #else
-                let txtFont = NSFont.systemFont(ofSize: fretConfig.margin * 0.5)
+                let txtFont = NSFont.systemFont(ofSize: fretConfig.margin)
                 #endif
                 let txtRect = CGRect(x: 0, y: 0, width: stringConfig.margin, height: fretConfig.spacing)
                 let transX = stringConfig.margin / 5 + origin.x
