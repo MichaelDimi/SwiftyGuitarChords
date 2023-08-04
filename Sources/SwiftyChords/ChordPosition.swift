e595b98850ef755af55d709791e6f8d69cb6ae57
Michael Dimitrov <94137570+MichaelDimi@users.noreply.github.com>
2023-08-04 11:35:05 -0600

    Update ChordPosition.swift

--- Sources/SwiftyChords/ChordPosition.swift
+++ Sources/SwiftyChords/ChordPosition.swift
@@ -162,11 +162,11 @@ public struct ChordPosition: Codable, Identifiable, Equatable {
             if baseFret != 1 {
                 let txtLayer = CAShapeLayer()
                 #if os(iOS)
-                let txtFont = UIFont.systemFont(ofSize: fretConfig.margin * 1.5)
+                let txtFont = UIFont.systemFont(ofSize: fretConfig.margin * 1.5) // Changes the font size
                 #else
-                let txtFont = NSFont.systemFont(ofSize: fretConfig.margin * 1.5)
+                let txtFont = NSFont.systemFont(ofSize: fretConfig.margin * 1.5) // Changes the font size
                 #endif
-                let txtRect = CGRect(x: 0, y: 0, width: stringConfig.margin, height: fretConfig.spacing)
+                let txtRect = CGRect(x: 0, y: 0, width: stringConfig.margin + 10, height: fretConfig.spacing + 10) // When font size is changed, must also change frame
                 let transX = stringConfig.margin / 5 + origin.x + 10 // Changes the x-pos of the fret number
                 let transY = origin.y + (fretConfig.spacing / 2) + fretConfig.margin
                 let txtPath = "\(baseFret)".path(font: txtFont, rect: txtRect, position: CGPoint(x: transX, y: transY))
